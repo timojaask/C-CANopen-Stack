d04708472edb9d6b3314d948d0c5c211f7b1c5e4
timojaask <timojaask@gmail.com>
2013-10-20 22:18:41 +0300

    Initial commit

---
+++ CCanOpenStack/CCanOpenStack/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  CCanOpenStack
+//
+//  Created by Timo Jääskeläinen on 20.10.2013.
+//  Copyright (c) 2013 Timo Jääskeläinen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+

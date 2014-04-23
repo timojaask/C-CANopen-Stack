a7271139d1910781b4cdfd5f92a3e8350b94e232
Timo <timojaask@gmail.com>
2014-04-23 11:49:31 +0300

    More comments added

--- CCanOpenStack/CCanOpenStack/sdo.h
+++ CCanOpenStack/CCanOpenStack/sdo.h
@@ -62,23 +62,109 @@ typedef enum {
 } sdo_abort_code;
 
 /***************************** Global Prototypes *****************************/
+/**
+ * @brief Parses a server command specifier from a raw CAN message
+ * @param message A raw CAN message containing the SDO message
+ * @return Returns server command specifier
+ */
 extern sdo_server_command sdo_get_server_command(can_message *message);
+/**
+ * @brief Parses a client command specifier from a raw CAN message
+ * @param message A raw CAN message containing the SDO message
+ * @return Returns client command specifier
+ */
 extern sdo_client_command sdo_get_client_command(can_message *message);
+/**
+ * @brief Parses a is_expedited bit from a raw CAN message
+ * @param message A raw CAN message containing the SDO message
+ * @return Returns the bit containing is_expedited information
+ */
 extern uint8_t sdo_get_expedited_bit(can_message *message);
+/**
+ * @brief Parses a size_indicated bit from a raw CAN message
+ * @param message A raw CAN message containing the SDO message
+ * @return Returns the bit containing size_indicated information
+ */
 extern uint8_t sdo_get_size_indicated_bit(can_message *message);
+/**
+ * @brief Parses data_size bits from a raw CAN message and returns actual data size in bytes
+ * @param message A raw CAN message containing the SDO message
+ * @return Returns actual data size in bytes
+ */
 extern int sdo_get_expedited_data_size(can_message *message);
+/**
+ * @brief Parses index bits from a raw CAN message
+ * @param message A raw CAN message containing the SDO message
+ * @return Returns object dictionary index
+ */
 extern uint16_t sdo_get_index(can_message *message);
+/**
+ * @brief Parses sub index bits from a raw CAN message
+ * @param message A raw CAN message containing the SDO message
+ * @return Returns object dictionary sub index
+ */
 extern uint8_t sdo_get_sub_index(can_message *message);
+/**
+ * @brief Parses data bits from a raw CAN message
+ * @param message A raw CAN message containing the SDO message
+ * @return Returns object dictionary data
+ */
 extern uint32_t sdo_get_expedited_data(can_message *message);
+/**
+ * @brief Initializes an abort transfer message and writes it into a raw CAN message
+ * @param message pointer to a raw CAN message where the abort transfer message will be written
+ * @param index Object dictionary index
+ * @param sub_index Object dictionary sub index
+ * @param abort_code SDO abort code
+ */
 extern void sdo_set_message_abort_transfer_data(can_message *message, uint16_t index, uint8_t sub_index, uint32_t abort_code);
+/**
+ * @brief Sets the is_expedited bit and writes it into the raw CAN message
+ * @param message pointer to a raw CAN message where the is_expedited bit will be written
+ * @param expedited The value of is_expedited bit
+ */
 extern void sdo_set_expedited_bit(can_message *message, uint8_t expedited);
+/**
+ * @brief Sets the size_indicated bit and writes it into the raw CAN message
+ * @param message pointer to a raw CAN message where the size_indicated bit will be written
+ * @param size_indicated The value of size_indicated bit
+ */
 extern void sdo_set_size_indicated_bit(can_message *message, uint8_t size_indicated);
-/* Note: Also sets data size indicated bit */
+/**
+ * @brief Sets the data size bits and writes it into the raw CAN message (also sets size_indicated bit to 1)
+ * @param message pointer to a raw CAN message where the data size bits will be written
+ * @param data_size Data size in bytes
+ */
 extern void sdo_set_expedited_data_size(can_message *message, int data_size);
+/**
+ * @brief Sets the OD index bits and writes it into the raw CAN message
+ * @param message pointer to a raw CAN message where the OD index bits will be written
+ * @param index The object dictionary index
+ */
 extern void sdo_set_index(can_message *message, uint16_t index);
+/**
+ * @brief Sets the OD sub index bits and writes it into the raw CAN message
+ * @param message pointer to a raw CAN message where the OD sub index bits will be written
+ * @param sub_index The object dictionary sub index
+ */
 extern void sdo_set_sub_index(can_message *message, uint8_t sub_index);
+/**
+ * @brief Sets the OD object's data bits and writes it into the raw CAN message
+ * @param message pointer to a raw CAN message where the OD object's data bits will be written
+ * @param data The object dictionary object's data
+ */
 extern void sdo_set_expedited_data(can_message *message, uint32_t data);
+/**
+ * @brief Parses the SDO command specifier bits from a raw can message
+ * @param message A raw CAN message containing the SDO
+ * @return Returns SDO command specifier
+ */
 extern int get_sdo_command(can_message *message);
+/**
+ * @brief Sets the SDO command specifier bits and writes it into the raw CAN message
+ * @param message pointer to a raw CAN message where the SDO command specifier bits will be written
+ * @param sdo_command The SDO command specifier
+ */
 extern void set_sdo_command(can_message *message, int sdo_command);
 
 #endif
